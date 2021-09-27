e6b4ef04593cd6a9b2b096e2517ab8c6514a0147
sandertoresen <sandertoresen@icloud.com>
2021-09-27 23:22:54 +0200

    schema complete, create and insert table functions complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from DbConnector import DbConnector
 from tabulate import tabulate
+# from datetime import datetime
 
 
 class ExampleProgram:
@@ -9,86 +10,114 @@ class ExampleProgram:
         self.db_connection = self.connection.db_connection
         self.cursor = self.connection.cursor
 
-    def create_table(self, table_name):
-        query = """CREATE TABLE IF NOT EXISTS %s (
-                   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-                   name VARCHAR(30))
-                """
-        # This adds table_name to the %s variable and executes the query
-        self.cursor.execute(query % table_name)
-        self.db_connection.commit()
-
-    def insert_data(self, table_name):
-        names = ['Bobby', 'Mc', 'McSmack', 'Board']
-        for name in names:
-            # Take note that the name is wrapped in '' --> '%s' because it is a string,
-            # while an int would be %s etc
-            query = "INSERT INTO %s (name) VALUES ('%s')"
-            self.cursor.execute(query % (table_name, name))
-        self.db_connection.commit()
-
-    def fetch_data(self, table_name):
-        query = "SELECT * FROM %s"
-        self.cursor.execute(query % table_name)
-        rows = self.cursor.fetchall()
-        print("Data from table %s, raw format:" % table_name)
-        print(rows)
-        # Using tabulate to show the table in a nice way
-        print("Data from table %s, tabulated:" % table_name)
-        print(tabulate(rows, headers=self.cursor.column_names))
-        return rows
-
     def drop_table(self, table_name):
         print("Dropping table %s..." % table_name)
-        query = "DROP TABLE %s"
+        query = """DROP TABLE IF EXISTS %s"""
         self.cursor.execute(query % table_name)
 
     def show_tables(self):
         self.cursor.execute("SHOW TABLES")
         rows = self.cursor.fetchall()
         print(tabulate(rows, headers=self.cursor.column_names))
+    
+    def print_table(self, table_name):
+        self.cursor.execute(f"SELECT * FROM {table_name}")
+        rows = self.cursor.fetchall()
+        print(tabulate(rows, headers=self.cursor.column_names))
 
-    def create_user(self, table_name):
+    def create_user_table(self):
         """Create the user table"""
-        query = """CREATE TABLE IF NOT EXISTS %s (
-                    id varchar(25) NOT NULL PRIMARY KEY,
-                    has_labels BOOLEAN
+        query = """CREATE TABLE IF NOT EXISTS User (
+                    userID varchar(25) NOT NULL PRIMARY KEY UNIQUE,
+                    hasLabels BOOLEAN
                     )
                     """
-        # This adds table_name to the %s variable and executes the query
-        self.cursor.execute(query % table_name)
+
+        # Add table to database
+        self.cursor.execute(query)
         self.db_connection.commit()
 
-    def create_activity(self, table_name):
+    def create_activity_table(self):
         """Create the activity table"""
-        query = """CREATE TABLE IF NOT EXISTS %s (
-                    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-                    user_id varchar(25),
-                    CONSTRAINT fk_user
-                    FOREIGN KEY (user_id) 
-                        REFERENCES User(id)
-                    food varchar(25))
+        query = """CREATE TABLE IF NOT EXISTS Activity (
+                    activityID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                    userID varchar(25) NOT NULL,
+                    transportationMode varchar(25),
+                    startDatetime DATETIME,
+                    endDatetime DATETIME,
+                    CONSTRAINT fk_user FOREIGN KEY (userID)
+                    REFERENCES User(userID))
                     """
-        # TODO add transportation_mode - string, start_date_time - datetime, end_date_time - datetime
 
-        # This adds table_name to the %s variable and executes the query
-        self.cursor.execute(query % table_name)
+        # Add table to database
+        self.cursor.execute(query)
+        self.db_connection.commit()
+
+    def create_trackpoint_table(self):
+        """Create the user table"""
+        # TODO how big should decimal's be?
+        total_numbers = 10 #total nr. of numbers in latitude
+        decimal_len = 5 # total nr. of decimals in latitude
+        
+        query = f"""CREATE TABLE IF NOT EXISTS TrackPoint (
+                    trackPointID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                    activityID INT NOT NULL,
+                    lat DOUBLE({total_numbers},{decimal_len}),
+                    lon DOUBLE({total_numbers},{decimal_len}),
+                    altitude INT,
+                    dateDays DOUBLE({total_numbers},{decimal_len}),
+                    date_time DATETIME,
+                    CONSTRAINT fk_activity FOREIGN KEY (activityID)
+                    REFERENCES Activity(activityID))
+                    """
+
+        # Add table to database
+        self.cursor.execute(query)
         self.db_connection.commit()
 
     def insert_user(self, name, has_labels):
-        """insert new user, name should be uniqe name which will be used as a primary key, has_tables should be TRUE or FALSE"""
+        """insert new user, into User table.
+        name should be uniqe nameID
+        has_lables should be "TRUE" or "FALSE"
+        """
 
-        query = f"""INSERT INTO User (id, has_labels) VALUES('{name}', {has_labels})"""
-        print(query)
+        query = f"""INSERT INTO User (userID, hasLabels) 
+                    VALUES('{name}', {has_labels})"""
+
         # This adds table_name to the %s variable and executes the query
         self.cursor.execute(query)
         self.db_connection.commit()
 
-    def insert_activity(self, name, food):
-        """insert new activity, name should be uniqe name which will be used as a primary key, has_tables should be TRUE or FALSE"""
+    def insert_activity(self, name, transportationMode, startDatetime, endDatetime):
+        """insert new activity, 
+        name should be a userID,
+        transportationMode should be string,
+        startDatetime should be Datetime,
+        endDatetime should be Datetime
+        
+        Datetime should have format:  "YYYY-MM-DD HH:MM:SS"
+        """
 
-        query = f"""INSERT INTO Activity (user_id, food) VALUES('{name}', '{food}')"""
-        print(query)
+        query = f"""INSERT INTO Activity(userID, transportationMode, startDatetime, endDatetime) 
+                    VALUES('{name}', '{transportationMode}', '{startDatetime}', '{endDatetime}')"""
+
+        # This adds table_name to the %s variable and executes the query
+        self.cursor.execute(query)
+        self.db_connection.commit()
+    
+    def insert_trackpoint(self, activityID, lat, lon, altitude, dateDays, date_time):
+        """insert new activity, 
+        activityID
+        lat (double)
+        lon (double)
+        altitude (int)
+        dateDays (double)
+        date_time (datetime)
+        """
+
+        query = f"""INSERT INTO TrackPoint(activityID, lat, lon, altitude, dateDays, date_time) 
+                    VALUES({activityID}, {lat}, {lon}, {altitude}, {dateDays}, '{date_time}')"""
+
         # This adds table_name to the %s variable and executes the query
         self.cursor.execute(query)
         self.db_connection.commit()
@@ -100,14 +129,11 @@ def main():
     program = ExampleProgram()
 
     # remove data incase error previous iteration
-    try:
-        program.drop_table("User")
-        program.drop_table("Activity")
-    except:
-        pass
+    program.drop_table("Activity")
+    program.drop_table("User")
 
-    program.create_user(table_name="User")
-    program.create_table(table_name="Activity")
+    program.create_user_table(table_name="User")
+    program.create_activity_table(table_name="Activity")
 
     program.insert_user('Hans', "TRUE")
     program.insert_activity("Hans", "Pizza")
@@ -116,8 +142,9 @@ def main():
     program.fetch_data("User")
     program.fetch_data("Activity")
 
-    program.drop_table("User")
     program.drop_table("Activity")
+    program.drop_table("User")
+
     # program.create_table(table_name="TrackPoint")
 
     # exit
