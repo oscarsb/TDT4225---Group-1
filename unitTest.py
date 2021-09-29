58d41df7c0252fe1200aa669829e98aca818fd9d
sandertoresen <sandertoresen@icloud.com>
2021-09-29 21:39:18 +0200

    changed tests to match input

--- unitTest.py
+++ unitTest.py
@@ -20,7 +20,7 @@ class DBhandler_Test:
 
         self.program.insert_trackpoint(activityID=1,
                                        lat=24.11, lon=11.24,
-                                       altitude=100, dateDays=12, date_time="1998-09-08 23:00:00")
+                                       altitude=100, date_time="1998-09-08 23:00:00")
 
     def _fetch_result(self):
         self.program.show_tables()
@@ -42,10 +42,11 @@ class DBhandler_Test:
         self._print_tables()
         self._remove_tables()
 
+
 class Datahandler_Test:
     def __init__(self):
         self.program = Datahandler()
-    
+
     def test_insert_userdata(self):
         """Test inserting user"""
         self.program.drop_tables()
@@ -53,13 +54,13 @@ class Datahandler_Test:
         self.program.insert_users()
         self.program.handler.print_table("User")
         self.program.drop_tables()
-    
+
+
 if __name__ == "__main__":
-    #dbhandler test
+    # dbhandler test
     DBtest = DBhandler_Test()
     DBtest.basic_test()
-    
-    #Datahandler test
+
+    # Datahandler test
     DATAtest = Datahandler_Test()
     DATAtest.test_insert_userdata()
-    
